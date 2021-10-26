ceb33b657c5be0ada882db93c77c8ec92ed6cc19
Michael Alpenfels <mwa172@uclive.ac.nz>
2021-10-27 00:05:23 +1300

    Login/Logout handling

--- src/main/java/com/github/thedeathlycow/resettablearenas/listeners/WorldListener.java
+++ src/main/java/com/github/thedeathlycow/resettablearenas/listeners/WorldListener.java
@@ -4,26 +4,31 @@ import com.github.thedeathlycow.resettablearenas.Arena;
 import com.github.thedeathlycow.resettablearenas.ArenaChunk;
 import com.github.thedeathlycow.resettablearenas.ResettableArenas;
 import org.bukkit.ChunkSnapshot;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 
 public class WorldListener implements Listener {
 
     private final ResettableArenas PLUGIN;
+    private static final String PLAYING_PREFIX = "playing_";
 
     public WorldListener() {
         PLUGIN = ResettableArenas.getInstance();
     }
 
     @EventHandler
-    public void onChunkLoad(ChunkLoadEvent event) {
-//        ChunkSnapshot chunk = event.getChunk().getChunkSnapshot();
-//
-//        ArenaChunk arenaChunk = PLUGIN.getDatabase().getArenaChunk(chunk);
-//
-//        if (arenaChunk != null) {
-//            arenaChunk.tick(PLUGIN.getDatabase());
-//        }
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+        for (String tag : player.getScoreboardTags()) {
+            if (tag.startsWith(PLAYING_PREFIX)) {
+                String arena = tag.substring(PLAYING_PREFIX.length());
+                String leaveTag = "leave_" + arena;
+                player.addScoreboardTag(leaveTag);
+                break;
+            }
+        }
     }
 }
